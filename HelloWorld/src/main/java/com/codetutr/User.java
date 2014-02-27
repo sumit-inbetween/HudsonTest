fcd39fdcdd8d15a38fe6a34dfc5fffba4595b697
sumit <sumit@inbetween.co.in>
2014-02-27 21:33:14 +0530

    done something for testing

--- HelloWorld/src/main/java/com/codetutr/User.java
+++ HelloWorld/src/main/java/com/codetutr/User.java
@@ -19,16 +19,5 @@ public String getFirstName(){
 		return firstName;
 		}
 		
-		public void setLastName(String lastName){
-	   this.lastName = lastName;
-	  }
-public String getlastName(){
-		return lastName;
-		}
-		public void setMidName(String midName){
-	   this.midName = midName;
-	  }
-public String getMidName(){
-		return midName;
-		}
+		
 }
