758c230b74ea9d6db40040636016fe4db85359a0
sumit <sumit@inbetween.co.in>
2014-02-27 13:22:21 +0530

    add a Person.java

---
+++ HelloWorld/src/main/java/com/codetutr/Person.java
@@ -0,0 +1,33 @@
+package com.codetutr;
+
+public class Person{
+	private String firstName;
+	private String lastName;
+	private String midName;
+	private String address;
+	public void setAddress(String address){
+	   this.address = address;
+	  }
+public String getAddress(){
+		return address;
+		}
+	public void setFirstName(String firstName){
+	   this.firstName = firstName;
+	  }
+public String getFirstName(){
+		return firstName;
+		}
+		
+		public void setLastName(String lastName){
+	   this.lastName = lastName;
+	  }
+public String getlastName(){
+		return lastName;
+		}
+		public void setMidName(String midName){
+	   this.midName = midName;
+	  }
+public String getMidName(){
+		return midName;
+		}
+}
